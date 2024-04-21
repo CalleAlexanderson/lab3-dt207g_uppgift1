ffc734bc754d20cb37394c133554c6685de10948
CalleAlexanderson <calle.alexanderson@telia.com>
2024-04-21 21:16:24 +0200

    get/post funkar

--- server.js
+++ server.js
@@ -14,6 +14,47 @@ mongoose.connect("mongodb://localhost:27017/dt208lab3").then(()=>{
     console.log("error when connecting to database: " + err);
 })
 
+const workPostSchema = new mongoose.Schema({
+    companyName:{
+        type: String, 
+        required: true
+    },
+    jobTitle:{
+        type: String, 
+        required: true
+    },
+    location:{
+        type: String, 
+        required: true
+    },
+    description:{
+        type: String, 
+        required: false
+    }
+});
+
+const workPost = mongoose.model("WorkPost", workPostSchema);
+
+app.get("/workposts", async(req, res) =>{
+    try {
+        let result = await workPost.find({});
+
+        return res.json(result);
+    } catch(err){
+        return res.status(500).json(err);
+    }
+})
+
+app.post("/workposts", async(req, res) =>{
+    try {
+        let result = await workPost.create(req.body);
+
+        return res.json(result);
+    } catch(err){
+        return res.status(400).json(err);
+    }
+})
+
 app.get("/api", (req,res) =>{
     res.json({message: "API is working, welcome"})
 })
